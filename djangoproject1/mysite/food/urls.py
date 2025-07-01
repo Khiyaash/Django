4cf1d3ef71e9ea6db3dcca95768e9846ad80f657
Sangeetha <sangeethasm1902@gmail.com>
2025-07-02 00:17:40 +0530

    added db

--- djangoproject1/mysite/food/urls.py
+++ djangoproject1/mysite/food/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('<int:item_id>/',views.detail,name='detail'),
     #add items
     path('add',views.CreateItem.as_view(),name='create_item'),
+    #update items
     path('update/<int:id>/',views.update_item,name='update_item'),
     path('delete/<int:id>/',views.delete_item,name='delete_item')
 ]
